9668cc63c4186c3f3ed354ed80fb1a4eed34ee5a
fdepuntis <77015977+fdepuntis@users.noreply.github.com>
2022-01-24 15:41:39 +0100

    Update StringUtilsTests.java

--- src/test/java/StringUtilsTests.java
+++ src/test/java/StringUtilsTests.java
@@ -1,46 +1,74 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+package fr.epsi.core;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.ValueSource;
+public final class StringUtils {
+    private static final int FIRST_CHAR = 'A';
+    private static final int LAST_CHAR = 'Z';
+    private static final int NB_CHARS = (LAST_CHAR - FIRST_CHAR) + 1;
 
-import fr.epsi.core.MathUtils;
-import fr.epsi.core.StringUtils;
-
-class StringUtilsTests {
-
-	@Test
-	void test() {
-
-	}
-
-	@ParameterizedTest(name = "{0} est pas palindrome") // {0} sera remplacé par le 1e argument
-	@ValueSource(strings = { "bob" }) // Le test sera lancé 6 fois
-	void isPalindrome(String phrase) {
-		assertTrue(StringUtils.isPalindrome(phrase));
-	}
-	
-	@ParameterizedTest(name = "{0} n'est pas palindrome") // {0} sera remplacé par le 1e argument
-	@ValueSource(strings = { "bobh" }) // Le test sera lancé 6 fois
-	void isNotPalindrome(String phrase) {
-		assertFalse(StringUtils.isPalindrome(phrase));
-	}
-
-	@ParameterizedTest(name = "{0} encodee :{1}  ")
-    @CsvFileSource(resources = "/cesar.csv", numLinesToSkip = 1)
-
-    void cesarEncode(String sentence, String request, int step) {
-        assertEquals(request, StringUtils.cesarEncode(sentence, step));
-
+    private StringUtils() {
     }
 
-    @ParameterizedTest(name = "{1} decodee  :{0}  ")
-    @CsvFileSource(resources = "/cesar.csv", numLinesToSkip = 1)
+    /**
+     * Encodes a sentence with Cesar with a specified step.
+     *
+     * @param sentence the sentence to decode.
+     * @param step     the step to use.
+     * @return the encoded sentence.
+     */
+    public static String cesarEncode(String sentence, int step) {
+        char[] decoded = new char[sentence.length()];
+        for (int i = 0; i < sentence.length(); i++) {
+            char currentChar = sentence.charAt(i);
+            if (currentChar == ' ' || currentChar == ',' || currentChar == '.') {
+                decoded[i] = currentChar;
+            } else {
+                decoded[i] = encrypt(currentChar, (step % NB_CHARS));
+            }
+        }
 
-    void cesarDecode(String sentence, String request, int step) {
-        assertEquals(sentence, StringUtils.cesarDecode(request, step));
+        return new String(decoded);
     }
+
+    /**
+     * Decodes a sentence with Cesar with a specified step.
+     *
+     * @param sentence the sentence to decode.
+     * @param step     the step to use.
+     * @return the decoded sentence.
+     */
+    public static String cesarDecode(String sentence, int step) {
+        return cesarEncode(sentence, -step);
+    }
+
+    /**
+     * Checks if the sentence is a palindrome.
+     *
+     * @param sentence the sentence to check.
+     * @return true if the sentence is a palindrome, false otherwise.
+     */
+    public static boolean isPalindrome(String sentence) {
+        sentence = sentence.replaceAll("\\s+", "");
+        char[] reverse = new char[sentence.length()];
+        int length = sentence.length();
+        for (int i = 0; i < length; i++) {
+            reverse[i] = sentence.charAt(length - i - 1);
+        }
+
+        return new String(reverse).equals(sentence);
+    }
+
+    /**
+     * Encrypts a character with a given step.
+     *
+     * @param character the character to encrypt.
+     * @param step      the step to use
+     * @return the encrypted character.
+     */
+    private static char encrypt(char character, int step) {
+        int charIndexInAlphabet = (character - FIRST_CHAR);
+        int encryptedCharIndex = charIndexInAlphabet + step + NB_CHARS;
+        return (char) ((encryptedCharIndex % NB_CHARS) + FIRST_CHAR);
+    }
+
+
 }
